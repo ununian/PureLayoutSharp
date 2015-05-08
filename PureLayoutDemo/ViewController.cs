6704ba368b701f9ea1f17559632a2083f794de0a
秋声赋 <unhappy224@163.com>
2015-05-08 15:28:45 +0800

    Initial check-in of module PureLayout

---
+++ PureLayoutDemo/ViewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using PureLayout;
+using UIKit;
+using CoreGraphics;
+
+namespace PureLayoutDemo
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+
+            var red = new UIView();
+            var blue = new UIView(); 
+
+            blue.BackgroundColor = UIColor.Blue; 
+            red.BackgroundColor = UIColor.Red;
+
+            this.View.Add(red);
+            this.View.Add(blue);
+
+            blue.AutoCenterInSuperview();
+            blue.AutoSetDimensionsToSize(new CGSize(50, 50));
+
+            red.AutoPinEdge(ALEdge.Top, ALEdge.Bottom, blue);
+            red.AutoPinEdge(ALEdge.Left, ALEdge.Right, blue);
+            red.AutoMatchDimension(ALDimension.Width, ALDimension.Width, blue);
+            red.AutoMatchDimension(ALDimension.Height, ALDimension.Height, blue);
+
+           
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ libPureLayout.linkwith.cs
@@ -0,0 +1,4 @@
+using System;
+using ObjCRuntime;
+
+[assembly: LinkWith ("libPureLayout.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
